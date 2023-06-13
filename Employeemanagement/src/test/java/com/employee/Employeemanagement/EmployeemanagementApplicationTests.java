c1243c5bbeceffed61bcf648ef7ff43e4eeaa483
srinila <srinila@192.168.140.240>
2023-06-13 13:50:45 +0530

    4th Commit

---
+++ Employeemanagement/src/test/java/com/employee/Employeemanagement/EmployeemanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.employee.Employeemanagement;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeemanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
